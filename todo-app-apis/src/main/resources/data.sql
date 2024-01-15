e655e6d262b29894d2872395420ffaf95c5cbc0f
Ashutosh Tiwari <ashutosh_tiwari@epam.com>
2024-01-15 14:15:45 +0530

    todo apis

---
+++ todo-app-apis/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into todo(id, description, done, target_date, username) values (10001,'Learn Aws', false, CURRENT_Date(), 'ashutosh');
+
+insert into todo(id, description, done, target_date, username) values (10002,'Learn JS', false, CURRENT_Date(), 'ashutosh');
+
+insert into todo(id, description, done, target_date, username) values (10003,'Learn Java', false, CURRENT_Date(), 'ashutosh');
